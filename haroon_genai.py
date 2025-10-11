c6c001df716160d12c2933833945aad70f6f5f11
haroonsheriff <haroonsheriff0507@gmail.com>
2025-10-11 17:43:04 +0530

    Add files via upload

---
+++ haroon_genai.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from google import genai
+haroon = genai.Client(api_key="AIzaSyAk2kT0-dBYGW0HcjOSE5cYRkVbH2I8VIo")
+st.title('haroon new chatgpt')
+question = st.text_input("Enter some text")
+if st.button("send"):
+    response=haroon.models.generate_content(model="gemini-2.5-flash",contents=question)
+    st.write(response.text)
