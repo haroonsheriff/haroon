fbe2c1bc35d0f553ea1de490a52549209d2fe849
haroonsheriff <haroonsheriff0507@gmail.com>
2025-10-16 20:28:10 +0530

    Update haroon_genai.py

--- haroon_genai.py
+++ haroon_genai.py
@@ -3,6 +3,8 @@ from google import genai
 haroon = genai.Client(api_key="AIzaSyAk2kT0-dBYGW0HcjOSE5cYRkVbH2I8VIo")
 st.title('haroon new chatgpt')
 question = st.text_input("Enter some text")
+st.file_uploader('upload multiple files')
 if st.button("send"):
     response=haroon.models.generate_content(model="gemini-2.5-flash",contents=question)
     st.write(response.text)
+
